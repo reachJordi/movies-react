a6b934f92db40c4f5f22a01504d9b1e7584824be
Jordi <iateyourdog94@gmail.com>
2023-09-07 23:21:08 +0200

    animation

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,29 +4,68 @@ import { motion, AnimatePresence } from "framer-motion";
 const AnimeCarousel = ({ animeData }) => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState('left');
+
+
+  const slideVariants = {
+    hiddenRight: {
+      x: "100%",
+      opacity: 0,
+    },
+    hiddenLeft: {
+      x: "-100%",
+      opacity: 0,
+    },
+    visible: {
+      x: "0",
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.8,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   
   const handleNext = () => {
+    setDirection("right");
     setCurrentIndex((prevIndex) =>
       prevIndex + 1 === animeData.length ? 0 : prevIndex + 1
     );
   };
   
   const handlePrevious = () => {
+    setDirection("left");
     setCurrentIndex((prevIndex) =>
       prevIndex - 1 < 0 ? animeData.length - 1 : prevIndex - 1
     );
   };
   
   const handleDotClick = (index) => {
+    setDirection(index > currentIndex ? "right" : "left");
     setCurrentIndex(index);
   };
 
   return (
     <div className="carousel">
-      {<img
-        key={currentIndex}
-        src={animeData[currentIndex].image}
-      />}
+      {
+        <AnimatePresence>
+          <motion.img
+            key={currentIndex}
+            src={animeData[currentIndex].image}
+            variants={slideVariants}
+            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
+            animate="visible"
+            exit="exit"
+          />
+        </AnimatePresence>
+      }
       <div key={animeData[currentIndex]._id} className="anime-card">
         <h2>{animeData[currentIndex].title}</h2>
         <p>Alternative Titles: {animeData[currentIndex].alternativeTitles.join(', ')}</p>
